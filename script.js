789b1c3b2ff7b8765762b60bc4864c856033bf3f
Lucas Busetto <busettol@sheridancollege.ca>
2025-05-30 14:58:37 -0400

    added media query in css to make every section properly formatted on smaller screens

--- script.js
+++ script.js
@@ -46,24 +46,6 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-//section fade-in function
-// const observer = new IntersectionObserver((entries) => {
-//   entries.forEach(entry => {
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add('visible');
-//       observer.unobserve(entry.target); // optional for one-time animation
-//     }
-//   });
-// }, {
-//   threshold: 0.15
-// });
-
-// // Observe all sections with the fade-in class
-// document.querySelectorAll('.fade-in-section').forEach(section => {
-//   observer.observe(section);
-// });
-
-//updated functino. sections fade in and out. 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
